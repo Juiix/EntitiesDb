ea1f7bb7e41f74d948f5fea8e7ea15273b953181
Tucker Ulrich <5263190+Juiix@users.noreply.github.com>
2026-02-01 11:19:30 -0700

    fix(core): updated Tag attribute to public

--- src/EntitiesDb/Attributes/TagAttributeAttribute.cs
+++ src/EntitiesDb/Attributes/TagAttributeAttribute.cs
@@ -6,6 +6,6 @@ namespace EntitiesDb;
 /// Marks a component as a zero-size tag component
 /// </summary>
 [AttributeUsage(AttributeTargets.Struct)]
-internal sealed class TagAttribute : Attribute
+public sealed class TagAttribute : Attribute
 {
 }
