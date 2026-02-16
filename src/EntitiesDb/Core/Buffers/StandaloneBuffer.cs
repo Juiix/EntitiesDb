938f74d54c1d4511da8fe42d87497d0666145013
Tucker Ulrich <5263190+Juiix@users.noreply.github.com>
2026-02-16 03:12:31 -0700

    feat(buffer): added StandaloneBuffer<T> for test usage

---
+++ src/EntitiesDb/Core/Buffers/StandaloneBuffer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace EntitiesDb.Core.Buffers;
+
+public unsafe sealed class StandaloneBuffer<T> : IDisposable where T : unmanaged
+{
+	private IntPtr _owner;
+
+	public StandaloneBuffer(ReadOnlySpan<T> data = default)
+	{
+		int headerBytes = BufferHeader.DataOffset;
+		nint total = headerBytes + (ComponentMeta<T>.InternalCapacity * ComponentMeta<T>.ByteSize);
+		_owner = Marshal.AllocHGlobal(total);
+
+		var buffer = new WriteBuffer<T>(_owner.ToPointer());
+		buffer.Init(data);
+	}
+
+	public WriteBuffer<T> Buffer => new(_owner.ToPointer());
+
+	public void Dispose()
+	{
+		try { if (_owner != IntPtr.Zero) Buffer.Clear(); }
+		finally
+		{
+			if (_owner != IntPtr.Zero) Marshal.FreeHGlobal(_owner);
+			_owner = IntPtr.Zero;
+		}
+	}
+}
